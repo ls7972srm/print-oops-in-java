698aed7b44590cb486af802603db6c75ccb6aca0
ls7972srm <ls7972@srmist.edu.in>
2026-02-17 09:28:43 +0530

    Add files via upload

---
+++ app2.java
@@ -0,0 +1,35 @@
+public class app2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ",
+                "* * *",
+                "*   *",
+                "*   *",
+                "* * *"
+        ));
+
+        System.out.println(String.join("   ",
+                "* * *",
+                "*   *",
+                "*   *",
+                "* * *"
+        ));
+
+        System.out.println(String.join("   ",
+                "* * *",
+                "*   *",
+                "* * *",
+                "*    ",
+                "*    "
+        ));
+
+        System.out.println(String.join("   ",
+                "*****",
+                "*    ",
+                "*****",
+                "    *",
+                "*****"
+        ));
+    }
+}
